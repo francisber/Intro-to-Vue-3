7a0b1240a7decb17e5decd44c80ff9f933dce4a3
francisber <francisber172@gmail.com>
2021-07-11 13:59:27 +0200

    second video: vue object created and mounted this in a html page

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return {
+            product: 'Test',
+        }
+    }
+    
+})
+const p = Vue.createApp({
+    data(){
+        return {
+            description: 'This is a description',
+        }
+    }
+    
+})
