f8321385062e7cd4406b2e8f8bbe72cca87de50c
francisber <francisber172@gmail.com>
2021-07-12 15:36:45 +0200

    add a computed statement in app vue. changue the image and sotck with a function for that

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
 const app = Vue.createApp({
     data(){
         return {
+            cart:0,
             product: 'Test',
-            image:'./assets/images/socks_blue.jpg',
-            inventory:10,
+            //image:'./assets/images/socks_blue.jpg',
+            //inventory:0,
+            //inStock:false,
+            selectedVariant:0,
             details:['first element','second element','just for test'],
             variants:[
-                {id:'0001',color:'green'},
-                {id:'0002',color:'blue'}
+                {id:'0001',color:'green',image:'./assets/images/socks_green.jpg', quantity:10},
+                {id:'0002',color:'blue',image:'./assets/images/socks_blue.jpg', quantity:0}
             ]
         }
+    },
+    methods:{
+        addToCart(){
+            this.cart+=1
+        },
+        changeVariant(index){
+            this.selectedVariant=index
+            console.log(index)
+        }
+    },
+    computed:{
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
+        }
     }
-    
 })
 const p = Vue.createApp({
     data(){
